ba0101d680895825e0033b4c15322c179c6dc209
BiOksana <biokissme@gmail.com>
2025-02-27 12:52:20 +0100

    init commit

---
+++ src/main/java/de/telran/onlineshopgarden/OnlineShopGardenApplication.java
@@ -0,0 +1,13 @@
+package de.telran.onlineshopgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopGardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopGardenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/telran/onlineshopgarden/OnlineShopGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.onlineshopgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopGardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
