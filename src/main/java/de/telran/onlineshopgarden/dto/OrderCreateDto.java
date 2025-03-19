ce02120322dc6350eeae69ac93c2fe297dd3e97a
rajatabla <vgubinbox@gmail.com>
2025-03-19 16:44:21 +0100

    SCRUM-35: Validation added to UserUpdateDto, UserCreateDto, OrderCreateDto, UserController

--- src/main/java/de/telran/onlineshopgarden/dto/OrderCreateDto.java
+++ src/main/java/de/telran/onlineshopgarden/dto/OrderCreateDto.java
@@ -1,6 +1,11 @@
 package de.telran.onlineshopgarden.dto;
 
 import de.telran.onlineshopgarden.entity.enums.DeliveryMethod;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +18,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderCreateDto {
+
+    @NotEmpty(message = "Order must contain at least one item")
+    @Valid
     private List<OrderItemDto> items;
 
+    @NotBlank(message = "Delivery address must not be blank")
     private String deliveryAddress;
 
+    @NotNull(message = "Delivery method must not be null")
     private DeliveryMethod deliveryMethod;
 
+    @NotBlank(message = "Contact phone must not be blank")
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid contact phone format")
     private String contactPhone;
 }
--- src/main/java/de/telran/onlineshopgarden/dto/UserUpdateDto.java
+++ src/main/java/de/telran/onlineshopgarden/dto/UserUpdateDto.java
@@ -1,5 +1,8 @@
 package de.telran.onlineshopgarden.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,11 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserUpdateDto {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 2, max = 50, message = "Name length must be between 2 and 50 characters")
     private String name;
 
+    @NotBlank(message = "Phone number must not be blank")
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid phone number format")
     private String phone;
 }
