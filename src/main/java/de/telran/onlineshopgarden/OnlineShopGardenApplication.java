2edbfb1ed3e28b5803adf92ad9d2b8ea967aabc4
Yevhen Kariev <yevhenkariev@gmail.com>
2025-04-08 20:55:44 +0200

    SCRUM-61_Hiding-sensitive-data

--- src/main/java/de/telran/onlineshopgarden/OnlineShopGardenApplication.java
+++ src/main/java/de/telran/onlineshopgarden/OnlineShopGardenApplication.java
@@ -1,15 +1,27 @@
 package de.telran.onlineshopgarden;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableScheduling
 public class OnlineShopGardenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OnlineShopGardenApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+
+        Map<String, Object> envMap = dotenv.entries().stream()
+                .collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+
+        SpringApplication app = new SpringApplication(OnlineShopGardenApplication.class);
+        app.setDefaultProperties(envMap);
+        app.run(args);
     }
 
 }
