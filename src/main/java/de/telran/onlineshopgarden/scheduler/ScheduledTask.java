4bedd75787e93e49e9fa0825485f954dfc0f3c79
Yevhen Kariev <yevhenkariev@gmail.com>
2025-08-14 11:27:45 +0200

    SCHEDULING 1mal per Tag

--- src/main/java/de/telran/onlineshopgarden/scheduler/ScheduledTask.java
+++ src/main/java/de/telran/onlineshopgarden/scheduler/ScheduledTask.java
@@ -20,7 +20,7 @@ public class ScheduledTask {
         this.orderRepository = orderRepository;
     }
 
-    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.DAYS)
     public void taskChangeOrderStatusForDelivery() {
         List<Order> orders = orderRepository.findByStatusNotIn(List.of(OrderStatus.CANCELED, OrderStatus.DELIVERED));
         for (Order order : orders) {
